e1a20d6f4c8e47a0eea82bf5d4789748cc96cc6b
Hasibul Islam <hasibulislam1670@gmail.com>
2023-11-25 00:29:21 +0600

    update :: Home Page Design Update -onGoing

--- client/src/Layouts/Main.jsx
+++ client/src/Layouts/Main.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
+import HomeSideBar from "../Pages/Home/HomeSideBar/HomeSideBar";
 import NavBar from "../Pages/Shared/NavBar";
 import Footer from "./../Pages/Shared/Footer";
 
 const Main = () => {
-
   return (
-      <div>
-          <NavBar></NavBar>
-          <Outlet></Outlet>
-         <Footer></Footer>
-      </div>
+    <div>
+      <NavBar></NavBar>
+      <HomeSideBar />
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
   );
 };
 
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,25 +1,21 @@
 import { Helmet } from "react-helmet-async";
 
-import BabyCare from "./BabyCare";
-import Banner from "./Banner";
-import FiverAndPain from "./FiverAndPain";
-import PetMedicine from "./PetMedicine";
-import FAQ from "./FAQ";
-
 const Home = () => {
   return (
     <div className="bg-base-00">
       <Helmet>
         <title> E-Medicine | Home</title>
       </Helmet>
-      <Banner />
+
+      {/* <Banner />
+
       <p className="text-xl font-bold underline flex justify-center mt-5 mb-5">
         Our Products{" "}
-      </p>
-      <FiverAndPain />
+      </p> */}
+      {/* <FiverAndPain />
       <BabyCare />
       <PetMedicine />
-      <FAQ/>
+      <FAQ/> */}
     </div>
   );
 };
